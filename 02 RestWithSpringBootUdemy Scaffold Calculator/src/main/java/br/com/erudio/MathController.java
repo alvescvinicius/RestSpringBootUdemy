80c16d952e7d42549c7f86f8e95f76aa777c1a03
alvescvinicius <alvescvinicius@gmail.com>
2020-10-26 21:00:44 -0300

    Criado classes de controle de exception - BAD_REQUEST

--- 02 RestWithSpringBootUdemy Scaffold Calculator/src/main/java/br/com/erudio/MathController.java
+++ 02 RestWithSpringBootUdemy Scaffold Calculator/src/main/java/br/com/erudio/MathController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erudio.exception.UnsupportedMathOperationException;
+
 @RestController
 public class MathController {
 
@@ -12,7 +14,7 @@ public class MathController {
 	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
 		
 		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new Exception();
+			throw new UnsupportedMathOperationException("Please set a numeric value !");
 		}
 		
 		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
