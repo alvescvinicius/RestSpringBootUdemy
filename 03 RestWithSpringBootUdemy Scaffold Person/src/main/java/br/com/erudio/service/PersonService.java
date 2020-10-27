5ec0923859eb51a1798a50e898edb8bd65b9e6ea
alvescvinicius <alvescvinicius@gmail.com>
2020-10-27 00:09:19 -0300

    Metodo get com findAll.

--- 03 RestWithSpringBootUdemy Scaffold Person/src/main/java/br/com/erudio/service/PersonService.java
+++ 03 RestWithSpringBootUdemy Scaffold Person/src/main/java/br/com/erudio/service/PersonService.java
@@ -1,5 +1,7 @@
 package br.com.erudio.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -10,7 +12,7 @@ import br.com.erudio.model.Person;
 public class PersonService {
 
 	private final AtomicLong counter = new AtomicLong();
-	
+
 	public Person findById(String id) {
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
@@ -18,6 +20,25 @@ public class PersonService {
 		person.setLastName("Alves");
 		person.setAddress("São Gonçalo - Rio de Janeiro - Brasil");
 		person.setGender("Male");
-		return person ;
+		return person;
+	}
+
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name" + i);
+		person.setLastName("Last name" + i);
+		person.setAddress("Some - Brasil" + i);
+		person.setGender("Male" + i);
+		return person;
 	}
 }
