d1ecab83107f1ba85656897c5a6f89634f05b5fa
alvescvinicius <alvescvinicius@gmail.com>
2020-10-29 00:07:38 -0300

    Implementação de versionamento criando a rota v2 para o novo metodo create sem que o metodo anterior pare de funcionar.

--- 05 RestWithSpringBootUdemy Scaffold Versionamento/src/main/java/br/com/erudio/service/PersonService.java
+++ 05 RestWithSpringBootUdemy Scaffold Versionamento/src/main/java/br/com/erudio/service/PersonService.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.erudio.converter.DozerConverter;
+import br.com.erudio.converter.custom.PersonConverter;
 import br.com.erudio.data.model.Person;
 import br.com.erudio.data.vo.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.exception.ResourceNotFoundException;
 import br.com.erudio.repository.PersonRepository;
 
@@ -16,12 +18,22 @@ public class PersonService {
 
 	@Autowired
 	PersonRepository repository;
+	
+	@Autowired
+	PersonConverter converter;
 
 	public PersonVO create(PersonVO person) {
 		var entity = DozerConverter.parseObject(person, Person.class);
 		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
 		return vo;
 	}
+	
+	public PersonVOV2 createV2(PersonVOV2 person) {
+		var entity = converter.convertVOToEntity(person);
+		var vo = converter.convertEntityToVO(entity);
+		return vo;
+	}
+
 
 	public List<PersonVO> findAll() {
 		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
