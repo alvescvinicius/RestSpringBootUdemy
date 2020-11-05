a396af58f72443d60a2f60111e4f76433e6351f9
alvescvinicius <alvescvinicius@gmail.com>
2020-11-04 23:50:09 -0300

    Implementando o SecurityConfig

--- 12 RestWithSpringBootUdemy Scaffold JWT e Spring Security/src/main/java/br/com/erudio/Startup.java
+++ 12 RestWithSpringBootUdemy Scaffold JWT e Spring Security/src/main/java/br/com/erudio/Startup.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -11,7 +12,14 @@ import org.springframework.context.annotation.ComponentScan;
 public class Startup {
 	
 	public static void main(String[] args) {
+		
 		SpringApplication.run(Startup.class, args);
+		
+		// Para gerar senha ao iniciar (testes)
+		//BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		//String result = bCryptPasswordEncoder.encode("admin123");
+		//System.out.println("My hash" + result);
+		
 	}
 	
 }
