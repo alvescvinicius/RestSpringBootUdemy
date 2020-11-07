9cd4eee2583947ab1d7a0c33da581fe7157aa1e5
alvescvinicius <alvescvinicius@gmail.com>
2020-11-06 21:09:25 -0300

    Ajustando o Bug do Token não Autorizado

--- 12 RestWithSpringBootUdemy Scaffold JWT e Spring Security/src/main/java/br/com/erudio/security/jwt/JwtTokenProvider.java
+++ 12 RestWithSpringBootUdemy Scaffold JWT e Spring Security/src/main/java/br/com/erudio/security/jwt/JwtTokenProvider.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import br.com.erudio.exception.InvalidJwtAuthenticationException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -25,29 +26,27 @@ import io.jsonwebtoken.SignatureAlgorithm;
 public class JwtTokenProvider {
 	
 	@Value("${security.jwt.token.secret-key:secret}")
-	private String secretKey;
+	private String secretKey = "secret";
 	
-	@Value("${security.jwt.token.expire-lenght:3600000}")
-	private long validityInMilliseconds = 3600000; // 1h
+	@Value("${security.jwt.token.expire-length:3600000}")
+	private long validityInMilliseconds = 3600000; //1h
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
 	@PostConstruct
-	public void init() {
+	protected void init() {
 		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
 	}
 	
 	public String createToken(String username, List<String> roles) {
 		Claims claims = Jwts.claims().setSubject(username);
 		claims.put("roles", roles);
-	
-		// Calcula tempo de validade do token
+		
 		Date now = new Date();
 		Date validity = new Date(now.getTime() + validityInMilliseconds);
 		
-		return Jwts
-				.builder()
+		return Jwts.builder()
 				.setClaims(claims)
 				.setIssuedAt(now)
 				.setExpiration(validity)
@@ -56,43 +55,32 @@ public class JwtTokenProvider {
 	}
 	
 	public Authentication getAuthentication(String token) {
-		UserDetails userDetails = this.userDetailsService
-				.loadUserByUsername(getUsername(token));
-		return new UsernamePasswordAuthenticationToken(userDetails, userDetails.getAuthorities());
+		UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUsername(token));
+		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
 	}
 
 	private String getUsername(String token) {
-		return Jwts
-				.parser()
-				.setSigningKey(secretKey)
-				.parseClaimsJws(token)
-				.getBody()
-				.getSubject();
+		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
 	}
 	
 	public String resolveToken(HttpServletRequest req) {
-		String bearerToken = req.getHeader("Autorization");
-		if(bearerToken != null && bearerToken.startsWith("Bearer ")) {
-			return bearerToken.substring(7,bearerToken.length());
-		}
+		String bearerToken = req.getHeader("Authorization");
+		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7, bearerToken.length());
+		}		
 		return null;
 	}
 	
 	public boolean validateToken(String token) {
 		try {
-			
-			Jws<Claims> claims = Jwts
-					.parser()
-					.setSigningKey(secretKey)
-					.parseClaimsJws(token);
-			
-			if(claims.getBody().getExpiration().before(new Date())) {
+			Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+			if (claims.getBody().getExpiration().before(new Date())) {
 				return false;
 			}
-			
-			return true;	
-		} catch (Exception e) {
-			throw new InvalidJwtAuthenticationException("Expired or invalid token");
+			return true;
+		} catch (JwtException | IllegalArgumentException e) {
+			throw new InvalidJwtAuthenticationException("Expired or invalid JWT token");
 		}
-	}	
+	}
+
 }
