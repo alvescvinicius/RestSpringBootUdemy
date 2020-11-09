a0f0012b2dc1f13c6a65a3d24dd5326b3ed2ed08
alvescvinicius <alvescvinicius@gmail.com>
2020-11-09 00:15:26 -0300

    Definindo as Configurações

--- 14 RestWithSpringBootUdemy Scaffold JWT Upload Download/src/main/java/br/com/erudio/config/FileStorageConfig.java
+++ 14 RestWithSpringBootUdemy Scaffold JWT Upload Download/src/main/java/br/com/erudio/config/FileStorageConfig.java
@@ -1,5 +1,18 @@
 package br.com.erudio.config;
 
-public class FileStorageConfig {
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@ConfigurationProperties(prefix = "file")
+public class FileStorageConfig {
+	
+	private String uploadDir;
+
+	public String getUploadDir() {
+		return uploadDir;
+	}
+
+	public void setUploadDir(String uploadDir) {
+		this.uploadDir = uploadDir;
+	}
+	
 }
