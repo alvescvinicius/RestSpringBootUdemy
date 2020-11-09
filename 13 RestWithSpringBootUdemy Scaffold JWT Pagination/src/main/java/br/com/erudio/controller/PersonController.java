80d48bc1b5224d7861c16a648f6f431cf20e59b6
alvescvinicius <alvescvinicius@gmail.com>
2020-11-09 00:04:53 -0300

    Removendo Warnings - melhorando código

--- 13 RestWithSpringBootUdemy Scaffold JWT Pagination/src/main/java/br/com/erudio/controller/PersonController.java
+++ 13 RestWithSpringBootUdemy Scaffold JWT Pagination/src/main/java/br/com/erudio/controller/PersonController.java
@@ -38,14 +38,16 @@ public class PersonController {
 
 	@Autowired
 	private PersonService services;
+	
+	@Autowired
+	private PagedResourcesAssembler<PersonVO> assembler;
 
 	@ApiOperation(value = "Find all people")
 	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
-	public ResponseEntity<PagedResources<PersonVO>> findAll(
+	public ResponseEntity<?> findAll(
 			@RequestParam(value = "page", defaultValue = "0") int page,
 			@RequestParam(value = "limit", defaultValue = "12") int limit,
-			@RequestParam(value = "direction", defaultValue = "asc") String direction,
-			PagedResourcesAssembler assembler) {
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
 		
 		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
 		
@@ -55,17 +57,18 @@ public class PersonController {
 		persons.stream()
 				.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
 
-		return new ResponseEntity<>(assembler.toResource(persons), HttpStatus.OK);
+		PagedResources<?> resources = assembler.toResource(persons);
+		
+		return new ResponseEntity<>(resources, HttpStatus.OK);
 	}
 	
 	@ApiOperation(value = "Find all people")
 	@GetMapping(value = "/findPersonByName/{firstName}", produces = { "application/json", "application/xml", "application/x-yaml" })
-	public ResponseEntity<PagedResources<PersonVO>> findPersonByName (
+	public ResponseEntity<?> findPersonByName (
 			@PathVariable("firstName") String firstName,
 			@RequestParam(value = "page", defaultValue = "0") int page,
 			@RequestParam(value = "limit", defaultValue = "12") int limit,
-			@RequestParam(value = "direction", defaultValue = "asc") String direction,
-			PagedResourcesAssembler assembler) {
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
 		
 		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
 		
@@ -75,7 +78,10 @@ public class PersonController {
 		persons.stream()
 				.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
 
-		return new ResponseEntity<>(assembler.toResource(persons), HttpStatus.OK);
+		PagedResources<?> resources = assembler.toResource(persons);
+		
+		return new ResponseEntity<>(resources, HttpStatus.OK);
+		
 	}
 
 	// @CrossOrigin(origins = "http://localhost:8080") // habilitar para metodo e
